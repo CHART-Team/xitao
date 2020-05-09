0cf737dee52b0ac25470940c73334450f8338f68
Mustafa <musbar@gmail.com>
2020-05-09 13:36:56 +0200

    remove unnecessary timetables
    Former-commit-id: cf254792646561ca288102091652ade56a1dedfb
    Former-commit-id: e336c952c943caa842f0c651e58878f54e0d96ba
    Former-commit-id: ff5e3a9ee3fea2cf88c7a296e79395385c2d6a3f [formerly 6fcfba4a0ee096532e743b63367274faab880b43] [formerly 8dfcddc24e008cf000ef7609abcb58c254465e82 [formerly c11a2d5f75a270f167fa7d98cc41b54161a26428]] [formerly 88ad4c00e2bf1c1d8a58f10bc946e2c30ccdd13b [formerly 559d3c95545e3a9bacff710ef8a225fb7a138db8] [formerly 1691aa6f8458983940e89008d47f38f31712481d [formerly 48a1c82e300b5459abe19fd59e097b1ad4f6dc2b]]]
    Former-commit-id: b896135b226c87b85bf4bbf8433ddb22f9197e84 [formerly bc0f6ed786d539f1364c6ca3040e276419280220] [formerly 53caf51ad14cc89bd2faba8c458a252052a3a0b7 [formerly d329f7f847f69b35f25302b254e14643cc962009]]
    Former-commit-id: 892e29685b541f764c83e11e9803deccf73ec4d7 [formerly 9008d13c1622983b7eef92b3d82b6f583c290f3a]
    Former-commit-id: b9da7a4fbd7aa93def7011aa36840e93a90c7b2c

--- benchmarks/sparselu/sparselu_taos.h
+++ benchmarks/sparselu/sparselu_taos.h
@@ -21,14 +21,6 @@ using namespace std;
 class LU0 : public AssemblyTask 
 {
 public: 
-#if defined(CRIT_PERF_SCHED)
-  //! A public static class variable.
-  /*!
-    It holds the performance trace table for the corresponding TAO.
-  */      
-  static float time_table[][XITAO_MAXTHREADS];
-#endif  
-  
   //! LU0 TAO constructor. 
   /*!
     \param _in is the input vector for which the elements should be accumulated
@@ -69,24 +61,12 @@ public:
   int BSIZE;     /*!< TAO implementation specific integer that holds the number of elements */
 };
 
-#if defined(CRIT_PERF_SCHED)
-float LU0::time_table[XITAO_MAXTHREADS][XITAO_MAXTHREADS]; 
-#endif
-
 
 /*! this TAO will take a set of doubles and add them all together
 */
 class FWD : public AssemblyTask 
 {
 public: 
-#if defined(CRIT_PERF_SCHED)
-  //! A public static class variable.
-  /*!
-    It holds the performance trace table for the corresponding TAO.
-  */      
-  static float time_table[][XITAO_MAXTHREADS];
-#endif  
-  
   //! FWD TAO constructor. 
   /*!
     \param _in is the input vector for which the elements should be accumulated
@@ -126,24 +106,11 @@ public:
   int BSIZE;     /*!< TAO implementation specific integer that holds the number of elements */
 };
 
-#if defined(CRIT_PERF_SCHED)
-float FWD::time_table[XITAO_MAXTHREADS][XITAO_MAXTHREADS]; 
-#endif
-
-
 /*! this TAO will take a set of doubles and add them all together
 */
 class BDIV : public AssemblyTask 
 {
 public: 
-#if defined(CRIT_PERF_SCHED)
-  //! A public static class variable.
-  /*!
-    It holds the performance trace table for the corresponding TAO.
-  */      
-  static float time_table[][XITAO_MAXTHREADS];
-#endif  
-  
   //! BDIV TAO constructor. 
   /*!
     \param _in is the input vector for which the elements should be accumulated
@@ -186,24 +153,11 @@ public:
   int BSIZE;     /*!< TAO implementation specific integer that holds the number of elements */
 };
 
-#if defined(CRIT_PERF_SCHED)
-float BDIV::time_table[XITAO_MAXTHREADS][XITAO_MAXTHREADS]; 
-#endif
-
-
 /*! this TAO will take a set of doubles and add them all together
 */
 class BMOD : public AssemblyTask 
 {
 public: 
-#if defined(CRIT_PERF_SCHED)
-  //! A public static class variable.
-  /*!
-    It holds the performance trace table for the corresponding TAO.
-  */      
-  static float time_table[][XITAO_MAXTHREADS];
-#endif  
-  
   //! BMOD TAO constructor. 
   /*!
     \param _in is the input vector for which the elements should be accumulated
@@ -244,77 +198,3 @@ public:
   double *inner; /*!< TAO implementation specific double point to the summation*/
   int BSIZE;     /*!< TAO implementation specific integer that holds the number of elements */
 };
-
-#if defined(CRIT_PERF_SCHED)
-float BMOD::time_table[XITAO_MAXTHREADS][XITAO_MAXTHREADS]; 
-#endif
-
-
-///*! this TAO will take a set of doubles and add them all together
-//*/
-//class BMODNA : public AssemblyTask 
-//{
-//public: 
-//#if defined(CRIT_PERF_SCHED)
-//  //! A public static class variable.
-//  /*!
-//    It holds the performance trace table for the corresponding TAO.
-//  */      
-//  static float time_table[][XITAO_MAXTHREADS];
-//#endif  
-//  
-//  //! BMOD TAO constructor. 
-//  /*!
-//    \param _in is the input vector for which the elements should be accumulated
-//    \param _out is the output element holding the summation     
-//    \param _len is the length of the vector 
-//    \param width is the number of resources used by this TAO
-//  */    
-//  BMODNA(double *_in1, double *_in2,  double *_out, int _len, int width) :
-//        row(_in1), col(_in2), inner(_out), BSIZE(_len), AssemblyTask(width) 
-//  {  
-//    int i,j;
-//    double *p;
-//
-//    p = (double*)malloc(BSIZE*BSIZE*sizeof(double));
-//    out = p;
-//    if (p!=NULL){
-//       for (i = 0; i < BSIZE; i++) 
-//          for (j = 0; j < BSIZE; j++){(*p)=(ELEM)0.0; p++;}
-//      
-//    }
-//    else printf ("OUT OF MEMORY!!!!!!!!!!!!!!!\n");
-//  }
-//  //! Inherited pure virtual function that is called by the runtime to cleanup any resources (if any), held by a TAO. 
-//  void cleanup() {     
-//  }
-//
-//  //! Inherited pure virtual function that is called by the runtime upon executing the TAO. 
-//  /*!
-//    \param threadid logical thread id that executes the TAO. For this TAO, we let logical core 0 only do the addition to avoid reduction
-//  */
-//  void execute(int threadid)
-//  {
-//    // let the leader do all the additions, 
-//    // otherwise we need to code a reduction here, which becomes too ugly
-//    std::cout << "BMOD tid: " << threadid << std::endl;
-//    if(threadid != leader) return;
-//    int i, j, k;
-//                                                                                  
-//    for (i=0; i<BSIZE; i++)
-//       for (k=0; k<BSIZE; k++) 
-//          for (j=0; j<BSIZE; j++)
-//             inner[i*BSIZE+j] = inner[i*BSIZE+j] - row[i*BSIZE+k]*col[k*BSIZE+j];
-//          
-//  }
-//  
-//  double *row;  /*!< TAO implementation specific double vector that holds the input to be accumulated */
-//  double *col;  /*!< TAO implementation specific double vector that holds the input to be accumulated */
-//  double *inner; /*!< TAO implementation specific double point to the summation*/
-//  int BSIZE;     /*!< TAO implementation specific integer that holds the number of elements */
-//};
-//
-//#if defined(CRIT_PERF_SCHED)
-//float BMODNA::time_table[XITAO_MAXTHREADS][XITAO_MAXTHREADS]; 
-//#endif
-//
