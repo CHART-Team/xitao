3a88e4879e07cf8cf6211597030f58bf186ed7ea
Mustafa <musbar@gmail.com>
2020-12-14 13:59:44 +0100

    remove unneeded files

--- src/queue_manager.cpp
+++
@@ -1,8 +0,0 @@
-#include "queue_manager.h"
-
-// template<> std::vector<PolyTask *> default_queue_manager::worker_ready_q[XITAO_MAXTHREADS];
-// // LFQueue<PolyTask *> queue_manager::worker_assembly_q[XITAO_MAXTHREADS];  
-// template<> std::queue<PolyTask *> default_queue_manager::worker_assembly_q[XITAO_MAXTHREADS];  
-
-
- 
